a15e0009fb68527557ed4ee7f3eebc46e166235b
FlyPocker <michas.rudzik@gmail.com>
2026-09-14 14:11:08 +0200

    testing bad health

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...') // uncomment this line to test health check
+  res.send('ok')
+})
 
 const start = async () => {
   await app.listen(PORT)
